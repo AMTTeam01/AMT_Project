1bbf44d3cdb2327e8729337775917192e137bb94
StéphaneB <stephane@natures.ch>
2020-09-18 16:15:26 +0200

    + Profile Servlet on /my_profile + profile.jsp

--- src/main/java/ch/heigvd/amt/mvcProject/presentation/CounterServlet.java
+++ src/main/java/ch/heigvd/amt/mvcProject/presentation/CounterServlet.java
@@ -6,12 +6,17 @@ import javax.servlet.http.HttpServletRequest; import javax.servlet.http.HttpServ
 
 @WebServlet(name = "CounterServlet", urlPatterns = "/counter")
 public class CounterServlet extends HttpServlet {
+
   private int counter = 0;
+
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String command = request.getParameter("command"); if ("reset".equals(command)) {
+    String command = request.getParameter("command");
+
+    if ("reset".equals(command)) {
       counter = 0;
     } else {
       counter = counter + 1;
     }
     response.getWriter().println("this page has been accessed " + counter + " time(s).");
-  } }
+  }
+}
--- src/main/java/ch/heigvd/amt/mvcProject/presentation/MyProfileServlet.java
+++ src/main/java/ch/heigvd/amt/mvcProject/presentation/MyProfileServlet.java
@@ -1,23 +1,16 @@
 package ch.heigvd.amt.mvcProject.presentation;
 
-import ch.heigvd.amt.mvcProject.business.QuoteGenerator;
-import ch.heigvd.amt.mvcProject.model.Quote;
-
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-public class MyProfileServlet extends javax.servlet.http.HttpServlet {
+@WebServlet(name = "MyProfileServlet", urlPatterns = "/my_profile")
+public class MyProfileServlet extends HttpServlet {
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/myprofile.jsp").forward(request, response);
     }
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/pages/myprofile.jsp").forward(request, response);
-    }
-
-
 }
