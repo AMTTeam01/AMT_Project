173a0eeb3090cc31131c7cb0e9f5d79734ba86c2
StéphaneB <stephane@natures.ch>
2020-09-19 11:59:27 +0200

    + CSS Structure + Header / Footer [WIP]

--- src/main/java/ch/heigvd/amt/mvcProject/presentation/CounterServlet.java
+++ src/main/java/ch/heigvd/amt/mvcProject/presentation/CounterServlet.java
@@ -1,22 +1,26 @@
 package ch.heigvd.amt.mvcProject.presentation;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet; import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest; import javax.servlet.http.HttpServletResponse; import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "CounterServlet", urlPatterns = "/counter")
 public class CounterServlet extends HttpServlet {
 
-  private int counter = 0;
+    private int counter = 0;
 
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String command = request.getParameter("command");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String command = request.getParameter("command");
 
-    if ("reset".equals(command)) {
-      counter = 0;
-    } else {
-      counter = counter + 1;
+        if ("reset".equals(command)) {
+            counter = 0;
+        } else {
+            counter = counter + 1;
+        }
+        response.getWriter().println("this page has been accessed " + counter + " time(s).");
     }
-    response.getWriter().println("this page has been accessed " + counter + " time(s).");
-  }
 }
--- src/main/webapp/WEB-INF/views/myprofile.jsp
+++ src/main/webapp/WEB-INF/views/myprofile.jsp
@@ -1,5 +1,8 @@
-<html>
-  <body>
-    <h2>Username</h2>
-  </body>
-</html>
+
+<%@include file="fragments/header.jsp"%>
+
+<div>
+  <h2>Username : no login info...</h2>
+</div>
+
+<%@include file="fragments/footer.jsp"%>
