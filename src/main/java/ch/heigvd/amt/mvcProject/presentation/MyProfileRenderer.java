466bcfd2e5254f40847dfb08ee0cbea0dae79567
Simon <simon.mattei@heig-vd.ch>
2020-09-23 09:04:04 +0200

    deleted a couple files and renamed

--- src/main/java/ch/heigvd/amt/mvcProject/presentation/MyProfileRenderer.java
+++ src/main/java/ch/heigvd/amt/mvcProject/presentation/MyProfileRenderer.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "MyProfileServlet", urlPatterns = "/my_profile")
-public class MyProfileServlet extends HttpServlet {
+public class MyProfileRenderer extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/views/myprofile.jsp").forward(request, response);
