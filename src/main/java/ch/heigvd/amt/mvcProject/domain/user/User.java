e267e4346222d0f689b2bf2e056019ab454d416c
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-09-26 16:17:35 +0200

    add custom builder

--- src/main/java/ch/heigvd/amt/mvcProject/domain/user/User.java
+++ src/main/java/ch/heigvd/amt/mvcProject/domain/user/User.java
@@ -4,15 +4,31 @@ import ch.heigvd.amt.mvcProject.domain.IEntity;
 import lombok.*;
 
 @Data
-@Builder
+@Builder(toBuilder = true)
 public class User implements IEntity {
 
     @Setter(AccessLevel.NONE)
-    private UserId id = new UserId();
+    private UserId id;
 
     private String username;
 
     private String email;
 
     private String password;
+
+    public static class UserBuilder {
+
+        public User build(){
+            if(id == null){
+                id = new UserId();
+            }
+
+            if(email == null){
+                email = "";
+            }
+
+            return new User(id,username,email,password);
+        }
+    }
+
 }
