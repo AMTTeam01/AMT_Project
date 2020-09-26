859209556f5b32e79fa6c9b8618dd5e91e78fd8a
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-09-26 15:25:33 +0200

    add service registry

---
+++ src/main/java/ch/heigvd/amt/mvcProject/application/ServiceRegistry.java
@@ -0,0 +1,31 @@
+package ch.heigvd.amt.mvcProject.application;
+
+import ch.heigvd.amt.mvcProject.application.user.UserFacade;
+import ch.heigvd.amt.mvcProject.domain.user.IUserRepository;
+import ch.heigvd.amt.mvcProject.infrastructure.persistence.InMemoryUserRepository;
+
+public class ServiceRegistry{
+
+    private static ServiceRegistry singleton; // Code smell
+
+    private IUserRepository userRepository;
+    private UserFacade userFacade;
+
+    public static ServiceRegistry getServiceRegistry(){
+        if(singleton == null){
+            singleton = new ServiceRegistry();
+        }
+
+        return singleton;
+    }
+
+    public ServiceRegistry() {
+        singleton = this;
+        userRepository = new InMemoryUserRepository();
+        userFacade = new UserFacade(userRepository);
+    }
+
+    public UserFacade getUserFacade() {
+        return userFacade;
+    }
+}
