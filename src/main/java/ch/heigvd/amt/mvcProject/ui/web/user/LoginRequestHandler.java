be31285c1b3642c6badf3b7e9e8ff24e07d0fe38
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-09-26 16:18:57 +0200

    Create Login Request Handler

---
+++ src/main/java/ch/heigvd/amt/mvcProject/ui/web/user/LoginRequestHandler.java
@@ -0,0 +1,42 @@
+package ch.heigvd.amt.mvcProject.ui.web.user;
+
+import ch.heigvd.amt.mvcProject.application.ServiceRegistry;
+import ch.heigvd.amt.mvcProject.application.user.UserCommand;
+import ch.heigvd.amt.mvcProject.application.user.UserFacade;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginRequestHandler", urlPatterns = "/request.login")
+public class LoginRequestHandler extends HttpServlet {
+
+    private ServiceRegistry serviceRegistry;
+    private UserFacade userFacade;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        serviceRegistry = ServiceRegistry.getServiceRegistry();
+        userFacade = serviceRegistry.getUserFacade();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserCommand command = UserCommand.builder()
+                .username(req.getParameter("username"))
+                .email("")
+                .password(req.getParameter("password"))
+                .build();
+
+        if (userFacade.isUserExist(command.getUserId())) {
+            resp.sendRedirect(getServletContext().getContextPath());
+        } else {
+            resp.sendRedirect(
+                    getServletContext().getContextPath() + "/login?error=Your e-mail or your password is incorrect");
+        }
+    }
+}
