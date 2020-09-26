31c2ed3f5e40c6034bf0b1f9f5cf866f6d9aecdc
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-09-26 15:14:52 +0200

    Add newUser command class

---
+++ src/main/java/ch/heigvd/amt/mvcProject/application/user/NewUserCommand.java
@@ -0,0 +1,15 @@
+package ch.heigvd.amt.mvcProject.application.user;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Builder
+@Getter
+@EqualsAndHashCode
+public class NewUserCommand {
+
+    private String username;
+
+    private String password;
+}
