9949dba1f6916bd367bdd74381498ffba35d4c13
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-10-01 20:32:15 +0200

    Change test order

--- src/test/java/ch/heigvd/amt/mvcProject/application/question/QuestionFacadeTest.java
+++ src/test/java/ch/heigvd/amt/mvcProject/application/question/QuestionFacadeTest.java
@@ -2,27 +2,33 @@ package ch.heigvd.amt.mvcProject.application.question;
 
 
 import ch.heigvd.amt.mvcProject.application.ServiceRegistry;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class QuestionFacadeTest {
 
     private static ServiceRegistry serviceRegistry;
     private static QuestionFacade questionFacade;
 
     @BeforeAll
-    public static void init(){
+    public static void init() {
         serviceRegistry = ServiceRegistry.getServiceRegistry();
         questionFacade = serviceRegistry.getQuestionFacade();
+    }
+
+    @Test
+    @Order(1)
+    public void getQuestion_WhenEmpty_ReturnEmptyList() {
+        assertTrue(questionFacade.getQuestions(null).getQuestions().size() == 0);
     }
 
     @Test
     public void addQuestion() throws QuestionFailedException {
         QuestionCommand command = QuestionCommand.builder()
                 .title("Titre")
-                .description("bla")
+                .description("Description")
                 .ranking(1)
                 .build();
 
@@ -34,7 +40,5 @@ public class QuestionFacadeTest {
         assertEquals(command.getTitle(), view.getQuestions().get(0).getTitle());
     }
 
-    @Test void getQuestion_WhenEmpty_ReturnEmptyList(){
-            assertTrue(questionFacade.getQuestions(null).getQuestions().size()== 0);
-    }
+
 }
