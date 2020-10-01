93f22cda3618878732d52573389e596270a0eb8c
Simon <simon.mattei@heig-vd.ch>
2020-10-01 19:05:18 +0200

    added faker library & placeholder random username for now

--- e2e/register_login_test.js
+++ e2e/register_login_test.js
@@ -1,11 +1,23 @@
 Feature('sign up & login');
 
+//TODO : Change for data managment with faker library
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+var username = makeid(5);
+
 Scenario('test registration and login process', (I) => {
     //registering
     I.amOnPage('/register')
 
     //not matching passwords
-    I.fillField('#txt_username','123')
+    I.fillField('#txt_username', username)
     I.fillField('#txt_email','123@123')
     I.fillField('#txt_password',secret('12345'))
     I.fillField('#txt_cpassword',secret('123456'))
@@ -13,7 +25,7 @@ Scenario('test registration and login process', (I) => {
     I.waitForText('The password and the confirmation of the password aren\'t the same')
 
     //matching passwords
-    I.fillField('#txt_username','123')
+    I.fillField('#txt_username', username)
     I.fillField('#txt_email','123@123')
     I.fillField('#txt_password',secret('12345'))
     I.fillField('#txt_cpassword',secret('12345'))
@@ -24,13 +36,13 @@ Scenario('test registration and login process', (I) => {
     I.amOnPage('/login')
 
     //wrong login
-    I.fillField('#txt_username','123')
+    I.fillField('#txt_username', username)
     I.fillField('#txt_password',secret('123456'))
     I.click('Login')
     I.waitForText('Check of credentials failed')
 
     //right login
-    I.fillField('#txt_username','123')
+    I.fillField('#txt_username', username)
     I.fillField('#txt_password',secret('12345'))
     I.click('Login')
     I.dontSeeInCurrentUrl('/login')
