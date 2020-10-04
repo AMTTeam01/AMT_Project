60155b0a156db0f42a09bbe79dde9799e48b40a3
StéphaneB <stephane@natures.ch>
2020-10-04 15:22:36 +0200

    added library and user repository [WIP]

---
+++ src/main/java/ch/heigvd/amt/mvcProject/infrastructure/persistence/jdbc/JdbcUserRepository.java
@@ -0,0 +1,42 @@
+package ch.heigvd.amt.mvcProject.infrastructure.persistence.jdbc;
+
+import ch.heigvd.amt.mvcProject.domain.user.IUserRepository;
+import ch.heigvd.amt.mvcProject.domain.user.User;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@ApplicationScoped
+@Named("JdbcUserRepository")
+public class JdbcUserRepository implements IUserRepository {
+
+    @Resource(lookup = "jdbc/help2000DS")
+    DataSource dataSource;
+
+    public JdbcUserRepository() {
+    }
+
+    public JdbcUserRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        try {
+            PreparedStatement statement = dataSource.getConnection().prepareStatement(
+                    "SELECT * FROM users WHERE username = '" + username + "'"
+            );
+            statement.setString(1, username);
+            ResultSet rs = statement.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+}
