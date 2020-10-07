d2c03b1070cb77410d2b6560e57bb44608f96882
Simon <simon.mattei@heig-vd.ch>
2020-10-07 13:47:24 +0200

    fixed register button on login

--- src/main/java/ch/heigvd/amt/mvcProject/ui/web/question/QuestionRenderer.java
+++ src/main/java/ch/heigvd/amt/mvcProject/ui/web/question/QuestionRenderer.java
@@ -1,4 +1,17 @@
 package ch.heigvd.amt.mvcProject.ui.web.question;
 
-public class QuestionRenderer {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "QuestionRenderer", urlPatterns = "/question")
+public class QuestionRenderer extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getAttribute("id");
+        request.getRequestDispatcher("/WEB-INF/views/new_question.jsp").forward(request, response);
+    }
 }
