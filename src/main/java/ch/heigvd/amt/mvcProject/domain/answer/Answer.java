bf480ffb8c00ee695270a7e2eef28be7f6253a31
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-10-14 10:45:04 +0200

    add answer domain

--- src/main/java/ch/heigvd/amt/mvcProject/domain/answer/Answer.java
+++ src/main/java/ch/heigvd/amt/mvcProject/domain/answer/Answer.java
@@ -1,4 +1,53 @@
 package ch.heigvd.amt.mvcProject.domain.answer;
 
-public class Answer {
+import ch.heigvd.amt.mvcProject.domain.IEntity;
+import ch.heigvd.amt.mvcProject.domain.question.QuestionId;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Data
+@Builder(toBuilder = true)
+public class Answer implements IEntity<Answer, AnswerId> {
+
+    @Setter(AccessLevel.NONE)
+    private AnswerId id;
+
+    private String description;
+
+    private Date creationDate;
+
+    private QuestionId questionId;
+
+    @Override
+    public Answer deepClone() {
+        return this.toBuilder()
+                .id(new AnswerId(id.asString()))
+                .build();
+    }
+
+    public static class AnswerBuilder {
+        public Answer build(){
+            if(id == null){
+                id = new AnswerId();
+            }
+
+            if(description == null || description.isEmpty()){
+                throw new IllegalArgumentException("description is mandatory");
+            }
+
+            if (creationDate == null ){
+                throw new IllegalArgumentException("CreationDate is mandatory");
+            }
+
+            if (questionId == null ) {
+                throw new IllegalArgumentException("questionId is mandatory");
+            }
+
+            return new Answer(id, description, creationDate, questionId);
+        }
+    }
 }
