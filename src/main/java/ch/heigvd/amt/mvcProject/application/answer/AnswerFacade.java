30f4168ee808745c5aff4640b4ca20941b30f828
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-10-21 14:58:26 +0200

    add delet into the facade

--- src/main/java/ch/heigvd/amt/mvcProject/application/answer/AnswerFacade.java
+++ src/main/java/ch/heigvd/amt/mvcProject/application/answer/AnswerFacade.java
@@ -1,6 +1,7 @@
 package ch.heigvd.amt.mvcProject.application.answer;
 
 import ch.heigvd.amt.mvcProject.domain.answer.Answer;
+import ch.heigvd.amt.mvcProject.domain.answer.AnswerId;
 import ch.heigvd.amt.mvcProject.domain.answer.IAnswerRepository;
 
 
@@ -26,5 +27,9 @@ public class AnswerFacade {
         } catch (Exception e) {
             throw new AnswerFailedException(e.getMessage());
         }
+    }
+
+    public void deleteAnswer(AnswerId id){
+        answerRepository.remove(id);
     }
 }
