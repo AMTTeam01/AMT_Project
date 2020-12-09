dc539c6abbdbee45243210199a64bbcef7980059
StéphaneB <stephane@natures.ch>
2020-12-09 13:41:28 +0100

    [WIP] trying to debug the pipeline

--- run-e2e-tests.sh
+++ run-e2e-tests.sh
@@ -4,16 +4,15 @@
 docker-compose down
 rm -rf ./volumes/db
 docker-compose build
-docker-compose up -d db
+docker-compose up -d
 
 cd e2e/
 npm install
 npx codeceptjs run --steps
-
 cd ..
 
 # Restart web app and database
-docker-compose down
-rm -rf ./volumes/db
-docker-compose build
-docker-compose up -d db
+#docker-compose down
+#rm -rf ./volumes/db
+#docker-compose build
+#docker-compose up -d db
