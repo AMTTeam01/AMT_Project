7d2e88909a7497eeb15e59356010b8e2259710f1
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-12-18 09:26:16 +0100

    add comment

--- src/main/java/ch/heigvd/amt/mvcProject/ui/web/user/handler/LogoutRequestHandler.java
+++ src/main/java/ch/heigvd/amt/mvcProject/ui/web/user/handler/LogoutRequestHandler.java
@@ -15,9 +15,11 @@ public class LogoutRequestHandler extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // Invalid the session
         HttpSession session = req.getSession();
         session.invalidate();
 
+        // override the user and set the valid timeout to 0
         Cookie cookie = new Cookie("username", "");
         cookie.setMaxAge(0);
 
